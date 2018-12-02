4315bf207164e36ce96ef32e46a0d93a4534ef53
conradojordan <conrado.jordan@gmail.com>
2018-12-02 21:31:29 -0200

    Update day1part2bruteForce.py

--- day1part2bruteForce.py
+++ day1part2bruteForce.py
@@ -1,13 +1,20 @@
+# Brute force solution for part 2 of day 1
+# Saves initial time for execution time computing
 import time
 startTime = time.time()
+
+# Opens input file and saves frequency changes in a list
 file = open('input.txt')
 frequencyChanges = file.read()
 frequencyChangesList = map(int, frequencyChanges.split())
 
+# Initialize some variables
 currentFrequency = 0
 frequencyList = [currentFrequency]
 foundDuplicate = 0
 iteration = 0
+
+# Keeps saving all reached frequencies and checking if a frequency has already been reached before
 while (not foundDuplicate):
     iteration += 1
     for freqChange in frequencyChangesList:
@@ -19,7 +26,10 @@ while (not foundDuplicate):
             break
         frequencyList.append(currentFrequency)
     currentFrequency
+    
+    # Prints current iteration
     print('Iteration ' + str(iteration))
-    print('currentFrequency = ' + str(currentFrequency))
+
+# Solution and execution time of the program
 print('First repeated frequency: ' + str(firstDuplicateFrequency))
 print('Program execution time: ' + str(round(time.time() - startTime, 3)) + ' seconds')
