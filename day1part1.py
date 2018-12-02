52954c6ac19fac75580ade1a5d0bdf881d67886d
conradojordan <conrado.jordan@gmail.com>
2018-12-02 21:28:14 -0200

    Update day1part1.py

--- day1part1.py
+++ day1part1.py
@@ -1,7 +1,9 @@
+# Opens the input file and save the frequency changes in a list
 file = open('input.txt')
 frequencyChanges = file.read()
 frequencyChangesList = map(int, frequencyChanges.split())
 
+# Sums and subtracts the frequency changes until reaching the last frequency
 currentFrequency = 0
 for freqChange in frequencyChangesList:
     currentFrequency += freqChange
